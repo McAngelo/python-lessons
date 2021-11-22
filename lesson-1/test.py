b20028515398d1cb07b152b622157e0bf4639e68
Michael Johnson <mjohnson@seso.global>
2021-11-22 22:09:58 +0000

    --learnt something new with the QT desinger

---
+++ lesson-1/test.py
@@ -0,0 +1,13 @@
+#Add this to the generated file
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent=parent)
+        self.setupUi(self)
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    w = MainWindow()
+    w.show()
+    sys.exit(app.exec_())
