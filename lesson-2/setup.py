992178ee6e9c478e004d66b87f362c1effe17e58
Michael Johnson <mjohnson@seso.global>
2021-11-26 09:35:17 +0000

    --worked on the a news app

--- lesson-2/setup.py
+++ lesson-2/setup.py
@@ -28,7 +28,7 @@ setup(
     data_files=DATA_FILES,
     options={'py2app': OPTIONS},
     setup_requires=['py2app'],
-    version='1.0',
+    version='0.1.0',
     description='Managing your passwords',
     author='Michael Johnson',
     author_email='mcangelo200@gmail.com',
